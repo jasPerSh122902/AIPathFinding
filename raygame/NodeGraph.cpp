425cca2a68167088c86b9e18d3e65eb6ebdc267a
jasper <s218005@students.aie.edu>
2022-03-21 10:37:28 -0500

    more commting

--- raygame/NodeGraph.cpp
+++ raygame/NodeGraph.cpp
@@ -10,7 +10,7 @@ DynamicArray<NodeGraph::Node*> reconstructPath(NodeGraph::Node* start, NodeGraph
 	while (currentNode != start->previous)
 	{
 		currentNode->color = 0xFFFF00FF;
-		start->color = 0x00FFFFFF;
+		start->color = 0x00FF00FF;
 		path.insert(currentNode, 0);
 		currentNode = currentNode->previous;
 	}
@@ -81,31 +81,34 @@ DynamicArray<NodeGraph::Node*> NodeGraph::findPath(Node* start, Node* goal)
 	/// <param name="openList">This array or list is the first thing that is checked. Holds nodes that have not been touched</param>
 	/// <param name="closedList">Holds the nodes that have been touched so to preven overlaping</param>
 	DynamicArray<NodeGraph::Node*> openList, closedList = DynamicArray<NodeGraph::Node*>();
-
 	openList.addItem(start);
 	//has to check to see if the openlist is empty
 	while (openList.getLength() != 0)
 	{
+		
 		//sorts the openlist by there g score
 		sortGScore(openList);
 		//makes the currentnode to the openlist at the index 0
 		NodeGraph::Node* m_currentNode = openList[0];
+		//if the currentnode is not walkable
+		if (m_currentNode->walkable == false)
+			return DynamicArray<NodeGraph::Node*>();//dont move
 		//gos through the openlists edges 
-		for (int n = 0; n < openList[0]->edges.getLength(); n++)
+		for (int n = 0; n < m_currentNode->edges.getLength(); n++)
 		{
 			//target node is the opelist edges target
-			NodeGraph::Node* targetNode = openList[0]->edges[n].target;
+			NodeGraph::Node* targetNode = m_currentNode->edges[n].target;
 			//tries to see if the two list contains the target node
 			if (!closedList.contains(targetNode) && !openList.contains(targetNode))
 			{
 				//makes the gscore of the targetnode equal to the openlist index 0 gscorce + the edges index N of the openlist index 0 at  cost
-				targetNode->gScore = openList[0]->gScore + openList[0]->edges[n].cost;
-				targetNode->previous = openList[0];//set targetnodes previous to the openlist index 0
+				targetNode->gScore = m_currentNode->gScore + m_currentNode->edges[n].cost;
+				targetNode->previous = m_currentNode;//set targetnodes previous to the openlist index 0
 				openList.addItem(targetNode);//adds the targetnode to the openlist
 			}
 		}
-		closedList.addItem(openList[0]);
-		openList.remove(openList[0]);
+		closedList.addItem(m_currentNode);
+		openList.remove(m_currentNode);
 		//returns if the currentnode is the goal
 		if (m_currentNode == goal)
 			return reconstructPath(start, goal);
@@ -166,9 +169,10 @@ void NodeGraph::resetConnectedNodes(Node* node, DynamicArray<Node*>& resetList)
 
 	for (int i = 0; i < node->edges.getLength(); i++)
 	{
-		node->edges[i].target->gScore = 0;
-		node->edges[i].target->hScore = 0;
-		node->edges[i].target->fScore = 0;
+		//changes the colors of the edges
+		node->edges[i].target->gScore = 0x00FF00FF;
+		node->edges[i].target->hScore = 0xFF0000FF;
+		node->edges[i].target->fScore = 0x00FF00FF;
 		node->edges[i].target->color = 0xFFFFFFFF;
 
 		//Draw the target node
